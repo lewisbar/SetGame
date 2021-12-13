151eb98a6b4ba4457cf64bad8ac57d837569a5f9
Lennart Wisbar <lewisbar.git@gmail.com>
2021-12-13 23:40:17 +0100

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Lennart Wisbar on 13.12.21.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
