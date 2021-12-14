ace1afac93b507c1e6da5ebf2dd49a0077d6bf76
Lennart Wisbar <lewisbar.git@gmail.com>
2021-12-14 04:19:47 +0100

    Added a Card struct to the model

--- SetGame/SetGame.swift
+++ SetGame/SetGame.swift
@@ -12,4 +12,15 @@ struct SetGame {
     enum Shape: CaseIterable { case diamond, squiggle, oval }
     enum Shading: CaseIterable { case solid, striped, open }
     enum Color: CaseIterable { case red, green, purple }
+    
+    // TODO: Find a clever way to create 81 cards in a loop
+    
+    
+    
+    struct Card {
+        let number: Number
+        let Shape: Shape
+        let Shading: Shading
+        let Color: Color
+    }
 }
