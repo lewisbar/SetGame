77b93a3284b7127b9f76e30223c9a22ffc6a4326
Lennart Wisbar <lewisbar.git@gmail.com>
2021-12-14 04:23:14 +0100

    Create all 81 cards in a loop

--- SetGame/SetGame.swift
+++ SetGame/SetGame.swift
@@ -13,9 +13,19 @@ struct SetGame {
     enum Shading: CaseIterable { case solid, striped, open }
     enum Color: CaseIterable { case red, green, purple }
     
-    // TODO: Find a clever way to create 81 cards in a loop
-    
-    
+    let cards: [Card] = {
+        var cards = [Card]()
+        for number in Number.allCases {
+            for shape in Shape.allCases {
+                for shading in Shading.allCases {
+                    for color in Color.allCases {
+                        cards.append(Card(number: number, Shape: shape, Shading: shading, Color: color))
+                    }
+                }
+            }
+        }
+        return cards
+    }()
     
     struct Card {
         let number: Number
